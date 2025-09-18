3b85a009f61364ef468fb9fca784edb6c4b35034
hhuu773npp12-coder <hhuu773npp12@gmail.com>
2025-09-19 00:41:31 +0300

    Fix Android NDK version for CodeMagic build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.mssyb"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973" // تم تحديث NDK هنا
 
     defaultConfig {
         applicationId = "com.example.mssyb"
